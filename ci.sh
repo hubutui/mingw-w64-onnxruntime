8e2b0168dc9cffc0e353606a513a995ff0692fa9
Hu Butui <hot123tea123@gmail.com>
2021-11-23 00:01:14 +0800

    update build script

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ wget https://github.com/hubutui/mingw-w64-dlpack/releases/download/v0.6/mingw-w6
 wget https://github.com/hubutui/mingw-w64-dmlc-core/releases/download/v0.3/mingw-w64-x86_64-dmlc-core-0.3-1-any.pkg.tar.zst
 wget https://github.com/hubutui/mingw-w64-json/releases/download/v3.10.4/mingw-w64-x86_64-json-3.10.4-1-any.pkg.tar.zst
 wget https://github.com/hubutui/mingw-w64-mimalloc/releases/download/v2.0.3/mingw-w64-x86_64-mimalloc-2.0.3-1-any.pkg.tar.zst
+wget https://github.com/hubutui/mingw-w64-mp11/releases/download/v1.77.0/mingw-w64-x86_64-mp11-1.77.0-1-any.pkg.tar.zst
 wget https://github.com/hubutui/mingw-w64-onnx/releases/download/v1.10.2/mingw-w64-x86_64-onnx-1.10.2-1-any.pkg.tar.zst
 
 pacman -U --noconfirm \
@@ -14,10 +15,10 @@ pacman -U --noconfirm \
   mingw-w64-x86_64-dmlc-core-0.3-1-any.pkg.tar.zst \
   mingw-w64-x86_64-json-3.10.4-1-any.pkg.tar.zst \
   mingw-w64-x86_64-mimalloc-2.0.3-1-any.pkg.tar.zst \
+  mingw-w64-x86_64-mp11-1.77.0-1-any.pkg.tar.zst \
   mingw-w64-x86_64-onnx-1.10.2-1-any.pkg.tar.zst
 git clone https://github.com/microsoft/onnxruntime.git
 cd onnxruntime
-sed -i "s,boost_mp11,Boost," "cmake/CMakeLists.txt"
 git submodule update --init --recursive
 cmake \
   -B build-shared \
